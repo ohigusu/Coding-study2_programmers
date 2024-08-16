0009bd224d6678f59711c11fb83d1977f9f39c92
ohigusu <suyk52@gmail.com>
2024-08-16 16:16:44 +0900

    Update 최빈값 구하기.py

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
@@ -1,12 +1,10 @@
 def solution(array):
     hash = {}
     for i in array:
-        # key가 존재하지 않을 경우 기본값 0을 반환하여 문제를 해결
-        if i in hash:
-            hash[i] += 1
-        else:
-            hash[i] = 1
-    
+        # key가 존재하지 않을 경우 기본값 0 반환
+        if i in hash: hash[i] += 1
+        else: hash[i] = 1
+  
     # 최빈값 후보를 찾기
     max_count = max(hash.values())
     result = [k for k, v in hash.items() if v == max_count]

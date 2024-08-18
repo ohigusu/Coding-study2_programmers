699a5d1d2c7e64ee0d0aa133250dac28febd0de4
ohigusu <suyk52@gmail.com>
2024-08-18 23:57:24 +0900

    Update 배열 뒤집기.py

--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.py
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.py
@@ -1,3 +1,10 @@
 def solution(num_list):
     answer = num_list[::-1]
     return answer
+
+#sol2
+def solution(num_list):
+    result =[]
+    while(num_list):
+        result.append(num_list.pop())
+    return result

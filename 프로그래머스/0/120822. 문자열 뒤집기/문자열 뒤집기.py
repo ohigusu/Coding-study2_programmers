23562607799cd3add398c82532a2c9ef9ea2f1b1
ohigusu <suyk52@gmail.com>
2024-08-19 00:10:00 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.py
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.py
@@ -1,4 +1,8 @@
 def solution(my_string):
-    #answer = ''
-    
-    return my_string[::-1]
+    answer = ''
+
+    for i in range(len(my_string)-1, -1, -1) :
+        answer += my_string[i]
+    return answer
+
+

3d2de99aab4008724325c2b2e2886c7a1b9aaa5d
ohigusu <suyk52@gmail.com>
2024-08-19 00:02:20 +0900

    Update 특정 문자 제거하기.py

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.py
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.py
@@ -1,6 +1,10 @@
+#sol1
 def solution(my_string, letter):
     answer = ''
     for i in my_string:
         if i != letter:
             answer += i
     return answer
+#sol2
+def solution(my_string, letter):
+    return my_string.replace(letter, '')

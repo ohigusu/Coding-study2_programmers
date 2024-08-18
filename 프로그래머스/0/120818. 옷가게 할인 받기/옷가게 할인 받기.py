3bf745251d39432bd321f41c27ada91523350128
ohigusu <suyk52@gmail.com>
2024-08-18 23:50:17 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -0,0 +1,7 @@
+def solution(price):
+    if price >= 500000: answer = price * 0.8
+    elif price >= 300000: answer = price * 0.9
+    elif price >= 100000: answer = price * 0.95
+    elif price < 100000: answer = price
+    return int(answer)
+

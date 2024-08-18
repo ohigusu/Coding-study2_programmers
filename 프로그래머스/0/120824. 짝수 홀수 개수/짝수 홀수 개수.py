1eee37d2c9cb63788b6a8b129ea300607f33c85b
ohigusu <suyk52@gmail.com>
2024-08-19 00:15:00 +0900

    Update 짝수 홀수 개수.py

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -1,10 +1,15 @@
+#sol1
 def solution(num_list):
     answer,o,e = [],[],[]
     for i in num_list :
-        if i%2 == 0:
-            o.append(i)
-        else:
-            e.append(i)
+        if i%2 == 0: o.append(i)
+        else: e.append(i)
     answer.append(len(o))
     answer.append(len(e))
+    return answer
+#sol2
+def solution(num_list):
+    answer = [0,0]
+    for n in num_list:
+        answer[n%2]+=1
     return answer

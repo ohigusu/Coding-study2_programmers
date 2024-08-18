dc37116757b77c53b8e0bf77b3f6c9e84547b903
ohigusu <suyk52@gmail.com>
2024-08-19 00:11:56 +0900

    Update 문자열 뒤집기.py

--- 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.py
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.py
@@ -1,8 +1,12 @@
+#sol1:
+def solution(my_string):
+    return my_string[::-1]
+#sol2:
 def solution(my_string):
     answer = ''
-
     for i in range(len(my_string)-1, -1, -1) :
         answer += my_string[i]
     return answer
-
-
+#sol3:
+def solution(my_string):
+    return ''.join(reversed(my_string))

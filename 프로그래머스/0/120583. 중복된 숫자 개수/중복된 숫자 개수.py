939a95c635d1ce927e437191f09591aa6d7f9cda
ohigusu <suyk52@gmail.com>
2024-08-21 20:59:16 +0900

    Update 중복된 숫자 개수.py

--- 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.py
+++ 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.py
@@ -1,6 +1,11 @@
+#sol1
 def solution(array, n):
     answer = 0 
     for i in array:
         if i == n:
             answer += 1
     return answer
+#sol2
+def solution(numbers):
+    numbers.sort()
+    return numbers[-2] * numbers[-1]

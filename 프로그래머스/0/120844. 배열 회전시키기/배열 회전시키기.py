ed3609d968ca47f9dab6b5090715fc5094e83f7a
ohigusu <suyk52@gmail.com>
2024-08-25 23:58:00 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.py
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.py
@@ -1,11 +1,7 @@
 def solution(numbers, direction):
-    answer,n = [],len(numbers)
+    answer = []
     if direction == "right":
-        answer.append(numbers[n-1])
-        for i in range(0,n-1):
-            answer.append(numbers[i])
+        answer = [numbers[-1]] + numbers[:-1] 
     else:
-        for i in range(1,n):
-            answer.append(numbers[i])
-        answer.append(numbers[0]) 
+        answer = numbers[1:] + [numbers[0]]
     return answer

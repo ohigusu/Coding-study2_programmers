a1c332a64286bb3ee3034ea620bbddf77eeaa65e
ohigusu <suyk52@gmail.com>
2024-09-09 11:01:30 +0900

    [level 0] Title: 합성수 찾기, Time: 0.39 ms, Memory: 9.98 MB -BaekjoonHub

--- 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
@@ -1,9 +1,8 @@
 def solution(n):
     answer = 0
-    
     for num in range(4,n+1):
-        n = 1
-        for i in range(2,num+1):
+        n = 2
+        for i in range(2,num):
             if num % i == 0:
                 n += 1
         if n >= 3:

523a11b12aa2b5ab2b278ab57453284f317ddb05
ohigusu <suyk52@gmail.com>
2024-09-22 16:42:25 +0900

    Create sol2.sql

---
+++ 프로그래머스/3/151139. 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기/sol2.sql
@@ -0,0 +1,19 @@
+-- 대여 시작일을 기준으로 2022년 8월부터 2022년 10월까지 총 대여 횟수가 5회 이상인 자동차들
+WITH CTE AS (
+    SELECT CAR_ID
+    FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+    WHERE YEAR(START_DATE) = 2022 
+          AND MONTH(START_DATE) IN (8,9,10)
+    GROUP BY CAR_ID 
+    HAVING COUNT(*) >= 5 
+)
+-- 해당 기간 동안의 월별 자동차 ID 별 총 대여 횟수(컬럼명: RECORDS) 리스트 출력 
+SELECT MONTH(START_DATE) AS MONTH
+      ,CAR_ID
+      ,COUNT(*) AS RECORDS
+FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+WHERE (CAR_ID IN (SELECT CAR_ID FROM CTE))
+      AND MONTH(START_DATE) BETWEEN 8 AND 10            
+GROUP BY MONTH, CAR_ID
+ORDER BY MONTH, CAR_ID DESC 
+-- 월을 기준으로 오름차순 정렬, 월이 같다면 자동차 ID를 기준으로 내림차순 정렬

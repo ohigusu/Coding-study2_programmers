d47ad6264b58383b9d0e458ee2a46406c10923d2
KIM SUYEON <suyk52@gmail.com>
2025-01-05 15:12:25 +0900

    [level 0] Title: 안전지대, Time: 0.08 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120866. 안전지대/안전지대.py
+++ 프로그래머스/0/120866. 안전지대/안전지대.py
@@ -1,25 +1,17 @@
-def check(x,y,l,board):
-    x,y=x-1,y-1
-    for i in range(x,x+3):
-        for j in range(y,y+3):
-            if 0<=i<l and 0<=j<l:
-                board[i][j]=1  
-
 def solution(board):
     answer = 0
-    coor = []
-    l = len(board)
-    for i in range(l):
-        for j in range(l):
+    result = []
+    n = len(board)
+    for i in range(n):
+        for j in range(n):
             if board[i][j] == 1:
-                coor.append([i,j])
-    for c in coor:
-        x,y=c
-        check(x,y,l,board)
-
-    print(board)
-
+                result.append([i,j])
+    for coor in result:
+        x,y = coor
+        for i in range(x-1,x+2):
+            for j in range(y-1,y+2):
+                if 0 <= i < n and 0 <= j < n:
+                    board[i][j]=1  
     for b in board:
         answer += b.count(0)
-
     return answer

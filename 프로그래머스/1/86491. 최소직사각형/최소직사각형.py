76890c0a30fd53aff271bc5639d49e96837c3942
KIM SUYEON <suyk52@gmail.com>
2025-01-18 15:03:56 +0900

    [level 1] Title: 최소직사각형, Time: 3.21 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -1,10 +1,2 @@
 def solution(sizes):
-    answer = 0
-    pre_ma = max(sizes[0][0],sizes[0][1]) 
-    pre_mi = min(sizes[0][0],sizes[0][1]) 
-    for size in sizes:
-        ma, mi = max(size),min(size)
-        pre_ma = max(pre_ma,ma)
-        pre_mi = max(pre_mi,mi)
-    answer = pre_ma*pre_mi
-    return answer
+    return max(max(x) for x in sizes)*max(min(x) for x in sizes)

101143637f32934dfeea86bb04fb4f6b8067b975
KIM SUYEON <suyk52@gmail.com>
2025-01-23 00:31:54 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.37 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -5,17 +5,25 @@ def solution(n, arr1, arr2):
         i = n-1
         while i >= 0:
             if a >= 2**i and b >= 2**i: 
-                ans+= '#'
+                ans += '#'
                 a -= 2**i
                 b -= 2**i
             elif a >= 2**i: 
-                ans+= '#'
+                ans += '#'
                 a -= 2**i
             elif b >= 2**i: 
-                ans+= '#'
+                ans += '#'
                 b -= 2**i
             else:
                 ans += ' '
             i -= 1 
         answer.append(ans)
     return answer
+# sol2
+# def solution(n, arr1, arr2):
+#     answer = [] 
+#     for a, b in zip(arr1, arr2):
+#         binary_str = bin(a | b)[2:].zfill(n)  
+#         row = binary_str.replace('1', '#').replace('0', ' ')
+#         answer.append(row)
+#     return answer

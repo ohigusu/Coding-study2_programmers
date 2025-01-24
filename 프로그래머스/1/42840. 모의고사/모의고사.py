68608d81b08853dc2c4d32079688eb074f6954cc
KIM SUYEON <suyk52@gmail.com>
2025-01-24 15:13:07 +0900

    [level 1] Title: 모의고사, Time: 1.73 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -0,0 +1,16 @@
+def solution(answers):
+    answer, dic, pre = [], {}, 0
+    dic[1] = [1, 2, 3, 4, 5]
+    dic[2] = [2, 1, 2, 3, 2, 4, 2, 5]
+    dic[3] = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    
+    for key, value in dic.items():
+        cnt = sum(1 for i, j in zip(answers, value * (len(answers) // len(value) + 1)) if i == j)
+        
+        if cnt > pre:
+            pre = cnt
+            answer = [key]
+        elif cnt == pre:
+            answer.append(key)
+    
+    return answer

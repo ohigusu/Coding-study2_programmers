a4be28c53c3fde17d24badbbd16c81fee8f7818a
KIM SUYEON <suyk52@gmail.com>
2025-01-24 23:01:07 +0900

    [level 1] Title: 소수 만들기, Time: 10.66 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
@@ -0,0 +1,22 @@
+def solution(nums):
+    '''
+    홀수+홀수+짝수=짝수
+    짝수+짝수+짝수=짝수 
+    홀수+홀수+홀수=홀수, 홀수+짝수+짝수=홀수
+    '''
+    answer = 0
+    for i in range(0,len(nums)-2):
+        for j in range(i+1,len(nums)-1):
+            for k in range(j+1,len(nums)):
+                su = nums[i]+nums[j]+nums[k]
+                if su % 2 == 0:
+                    continue
+                tf = True
+                for de in range(3,int(su**0.5)+1,2):
+                    if su%de==0:
+                        tf=False
+                        break
+                if tf:
+                    answer += 1
+
+    return answer

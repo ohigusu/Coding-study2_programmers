99ad1a9f303f37393397061a0fde521afe06b702
KIM SUYEON <suyk52@gmail.com>
2025-01-26 01:29:07 +0900

    [level 1] Title: 덧칠하기, Time: 3.79 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
@@ -4,23 +4,9 @@ def solution(n, m, section):
     if (ma-mi)+1 <= m: 
         return answer
     elif (ma-mi)+1 > m:
-        if n-ma+1 >= m:
-            pass_i = mi+m-1
-            for i in section:
-                if i <= pass_i:
-                    continue
-                else:
-                    pass_i = i+m-1
-                    answer += 1
-        elif n-ma+1 < m:
-            pass_i = mi+m-1
-            for i in section:
-                if i <= pass_i:
-                    continue
-                elif i > pass_i and i+m-1 <= n:
-                    pass_i = i+m-1
-                    answer += 1
-                elif i > pass_i and i+m-1 > n:
-                    answer += 1 
-                    break
+        pass_i = mi + m - 1
+        for i in section:
+            if i > pass_i:
+                pass_i = i+m-1
+                answer += 1
     return answer

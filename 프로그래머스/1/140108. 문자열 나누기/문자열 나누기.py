90342bdccda522dfdc7ce881ffbef75c47d212de
KIM SUYEON <suyk52@gmail.com>
2025-01-27 00:11:40 +0900

    [level 1] Title: 문자열 나누기, Time: 1.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.py
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.py
@@ -1,23 +1,21 @@
 def solution(s):
-    answer = 0  # 분해된 문자열 개수
-    count_x, count_other = 0, 0  # 첫 글자의 개수와 다른 글자의 개수
-    first_char = None  # 현재 구간에서 첫 번째 등장한 문자
-
+    answer = 0  
+    count_x, count_other = 0, 0  
+    first_char = None  
+    
     for char in s:
         if first_char is None:
-            first_char = char  # 첫 문자 설정
+            first_char = char  
         if char == first_char:
             count_x += 1
         else:
             count_other += 1
 
-        # x의 개수와 다른 글자의 개수가 같아지면 분리
         if count_x == count_other:
             answer += 1
-            first_char = None  # 새로운 구간 시작
-            count_x, count_other = 0, 0  # 카운트 초기화
+            first_char = None  
+            count_x, count_other = 0, 0 
 
-    # 마지막 남은 문자열이 있다면 하나의 구간으로 추가
     if count_x != count_other:
         answer += 1
 

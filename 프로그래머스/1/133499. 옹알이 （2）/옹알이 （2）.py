2494670ea4dacf6c7f46cf6d4168830c30d49db7
KIM SUYEON <suyk52@gmail.com>
2025-01-26 19:27:13 +0900

    [level 1] Title: 옹알이 (2), Time: 2.84 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
@@ -14,3 +14,13 @@ def solution(babbling):
         if len(ans) == len(bab):
             answer += 1
     return answer
+
+# def solution(babbling):
+#     answer = 0
+#     for i in babbling:
+#         for j in ['aya','ye','woo','ma']:
+#             if j*2 not in i:
+#                 i=i.replace(j,' ')
+#         if len(i.strip())==0:
+#             answer +=1
+#     return answer

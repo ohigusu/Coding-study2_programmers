8c849e5714e97003ae8a8e6537ddc034725680e1
KIM SUYEON <suyk52@gmail.com>
2025-02-01 00:10:09 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 66.23 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,8 +1,10 @@
 from collections import Counter
 def solution(participant, completion):
-    count = Counter(participant)  
-    for name in completion:
-        count[name] -= 1  
-    for name, cnt in count.items():
-        if cnt > 0:
-            return name
+    # count = Counter(participant)  
+    # for name in completion:
+    #     count[name] -= 1  
+    # for name, cnt in count.items():
+    #     if cnt > 0:
+    #         return name
+    answer = Counter(participant) - Counter(completion)
+    return list(answer.keys())[0]

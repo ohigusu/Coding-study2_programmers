a1dd87775c2daf5ca6819d73e62735b1d4444aad
KIM SUYEON <suyk52@gmail.com>
2025-03-04 00:18:59 +0900

    [level 1] Title: 신고 결과 받기, Time: 2990.93 ms, Memory: 32.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -0,0 +1,29 @@
+def solution(id_list, report, k):
+    '''
+    이용자의 ID:id_list
+    각 이용자가 신고한 이용자의 ID 정보:report
+    정지 기준이 되는 신고 횟수:k
+    return
+    각 유저별로 처리 결과 메일을 받은 횟수
+    '''
+    n = len(id_list)
+    answer = [0]*n
+    result = [[0]*n for _ in range(n+1)]
+    for rep in report:
+        give, receive = rep.split(" ")
+        give_idx,receive_idx = id_list.index(give),id_list.index(receive)
+        
+        if result[give_idx][receive_idx] == 1:
+            pass
+        else:
+            result[give_idx][receive_idx] = 1
+            result[n][receive_idx] += 1
+    for idx,i in enumerate(result[n]):
+        if i < k:
+            pass
+        else:
+            for j in range(n):
+                if result[j][idx]:
+                    answer[j] += 1
+    return answer
+    

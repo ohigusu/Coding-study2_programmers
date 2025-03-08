7aba485b65bf88f53bf5131240645d3fd464e611
KIM SUYEON <suyk52@gmail.com>
2025-03-08 23:28:21 +0900

    [Silver V] Title: 단어 정렬, Time: 536 ms, Memory: 37024 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -3,8 +3,10 @@ li= []
 for _ in range(N):
 	word = input()
 	li.append(word)
+    
 set_li = list(set(li))
-set_li.sort(key=lambda x: (len(x), x))
+set_li.sort()
+set_li.sort(key=lambda x: len(x))
 
 for i in set_li:
 	print(i)

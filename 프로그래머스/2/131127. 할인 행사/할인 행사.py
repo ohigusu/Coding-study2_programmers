d9889b635cf2eea5553cb65ec5edc844df293949
KIM SUYEON <suyk52@gmail.com>
2025-03-21 16:37:16 +0900

    [level 2] Title: 할인 행사, Time: 184.94 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -1,13 +1,9 @@
 from collections import Counter
 def solution(want, number, discount):
     answer = 0
+    dic_want = dict(zip(want,number))
     for i in range(len(discount)-10+1):
         li_discount = Counter(discount[i:i+10])
-        conti = True
-        for name, num in zip(want,number):
-            if name not in li_discount or num > li_discount[name]:
-                conti = False
-                break
-        if conti:
-            answer += 1
+        if dic_want == li_discount:
+                answer += 1
     return answer

a5a5f4f24246f71d83c884372400e68bbeeb5234
KIM SUYEON <suyk52@gmail.com>
2025-03-22 01:24:18 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 9.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,10 +1,14 @@
 from collections import defaultdict
 def solution(clothes):
     answer = 1
-    appendix = defaultdict(list)
+    appendix = {}
     for i in range(len(clothes)):
-        appendix[clothes[i][1]].append(clothes[i][0])
+        kind = clothes[i][1]
+        if kind in appendix:
+            appendix[kind] += 1
+        else:
+            appendix[kind]=2
     for _,val in appendix.items():
-        answer *= len(val)+1
+        answer *= val
     answer -= 1
     return answer

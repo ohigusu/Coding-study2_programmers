14e54a2398ad49d5c10a3a126da2e627907d23d0
KIM SUYEON <suyk52@gmail.com>
2025-03-22 02:36:48 +0900

    [level 2] Title: H-Index, Time: 0.13 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,6 +1,4 @@
 def solution(citations):
-    n = len(citations)
-    answer = n
     citations_sort = sorted(citations,reverse=True) #내림차순
     for i, cite in enumerate(citations_sort):
         if cite <= i:

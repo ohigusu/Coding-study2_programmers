e90fe22ce9b496b017bf542fe9c281b54e71fced
KIM SUYEON <suyk52@gmail.com>
2025-04-07 23:37:02 +0900

    [Silver III] Title: 2×n 타일링, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -1,23 +1,10 @@
-def factorial(n):
-    result = 1
-    for i in range(2,n+1):
-        result *= i
-    return result
+n = int(input().strip())
+dp = [0]*(n+1)
 
-def nCr(n, r):
-    if r > n:
-        return 0
-    return factorial(n)//(factorial(r)*factorial(n-r))
+dp[0]=1
+if n>=1:
+    dp[1]=1
+for i in range(2,n+1):
+    dp[i] = dp[i-1]+dp[i-2]
 
-N = int(input().strip())
-answer = 0
-if N%2: # 홀수
-    end = ((N-1)//2)+1  
-else:
-    end = (N//2)+1 
-
-for k in range(0,end):
-    n,r = N-2*k,k
-    answer += nCr(n+r,r)
-
-print(answer%10007)
+print(dp[n]%10007)

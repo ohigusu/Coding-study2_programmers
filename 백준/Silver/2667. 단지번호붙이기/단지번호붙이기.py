2b582e351b0ffbac0fc6f3e0d73283c6445a904e
KIM SUYEON <suyk52@gmail.com>
2025-05-10 23:37:48 +0900

    [Silver I] Title: 단지번호붙이기, Time: 36 ms, Memory: 32504 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,30 +1,27 @@
-N = int(input())
+N = int(input().strip())
 graph = [list(map(int, input().strip())) for _ in range(N)]
-visited = [[False] * N for _ in range(N)]
+visited = [[0] * N for _ in range(N)]
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+dx=[-1,1,0,0]
+dy=[0,0,-1,1]
 
-def dfs(x, y):
-    count = 1
-    visited[x][y] = True
-
-    for d in range(4):
-        nx = x + dx[d]
-        ny = y + dy[d]
-
-        if 0 <= nx < N and 0 <= ny < N:
-            if graph[nx][ny] and not visited[nx][ny]:
-                count += dfs(nx, ny)
-    return count
+def dfs(x,y):
+    cnt = 1
+    visited[y][x] = 1
+    for i in range(4):
+        nx,ny = x + dx[i],y+dy[i]
+        if 0<= nx < N and 0<= ny < N:
+            if graph[ny][nx]==1 and not visited[ny][nx]:
+                cnt += dfs(nx,ny)
+    return cnt
 
 result = []
 
 for i in range(N):
     for j in range(N):
-        if graph[i][j] and not visited[i][j]:
-            result.append(dfs(i, j))
+        if graph[j][i]==1 and not visited[j][i]:
+            result.append(dfs(i,j))
 
-print(len(result))          # 단지 수
-for r in sorted(result):    # 단지별 집 수 (오름차순)
+print(len(result))
+for r in sorted(result):
     print(r)

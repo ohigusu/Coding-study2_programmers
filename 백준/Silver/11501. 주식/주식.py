023678a06d0dd554c7b7b09cb09c1dcfcee43808
KIM SUYEON <suyk52@gmail.com>
2025-05-30 13:40:25 +0900

    [Silver II] Title: 주식, Time: 2680 ms, Memory: 154732 KB -BaekjoonHub

---
+++ 백준/Silver/11501. 주식/주식.py
@@ -0,0 +1,16 @@
+T = int(input())
+
+for _ in range(T):
+    N = int(input())
+    prices = list(map(int, input().split()))
+    
+    max_price = 0
+    profit = 0
+    
+    for i in range(N - 1, -1, -1): 
+        if prices[i] > max_price:
+            max_price = prices[i]
+        else:
+            profit += max_price - prices[i]
+    
+    print(profit)

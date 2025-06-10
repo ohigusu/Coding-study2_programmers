ebc124a2d305424b16706b758ee4129610a13279
KIM SUYEON <suyk52@gmail.com>
2025-06-10 22:11:37 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 72.98 ms, Memory: 18.5 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
@@ -1,37 +1,21 @@
 from collections import deque
-
+from collections import deque
 def solution(queue1, queue2):
-    dq1 = deque(queue1)
-    dq2 = deque(queue2)
-    sum1 = sum(dq1)
-    sum2 = sum(dq2)
-
-    total = sum1 + sum2
-    # 1) 전체 합이 홀수면 불가능
-    if total % 2 != 0:
-        return -1
-
-    target = total // 2
-    cnt = 0
-    # 2) 안전한 최대 연산 횟수 (두 큐 길이의 3배 정도면 충분)
-    max_ops = len(queue1) * 3
-
-    # 3) pop + append 를 1회로 간주
-    while cnt < max_ops:
-        if sum1 == target:
-            return cnt
-        if sum1 < target:
-            # queue2 -> queue1
-            v = dq2.popleft()
-            sum2 -= v
-            dq1.append(v)
-            sum1 += v
+    queue1_sum = sum(queue1)
+    queue2_sum = sum(queue2)
+    queue1 = deque(queue1)
+    queue2 = deque(queue2)
+    for i in range(4 * len(queue1) + 1):
+        if queue1_sum > queue2_sum:
+            x = queue1.popleft()
+            queue1_sum -= x
+            queue2_sum += x
+            queue2.append(x)
+        elif queue1_sum < queue2_sum:
+            x = queue2.popleft()
+            queue2_sum -= x
+            queue1_sum += x
+            queue1.append(x)
         else:
-            # queue1 -> queue2
-            v = dq1.popleft()
-            sum1 -= v
-            dq2.append(v)
-            sum2 += v
-        cnt += 1
-
+            return i
     return -1

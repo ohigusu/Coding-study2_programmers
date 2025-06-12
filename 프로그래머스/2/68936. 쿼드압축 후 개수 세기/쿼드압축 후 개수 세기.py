19f1595811ee25215d85ec92f88d4cc6115b686a
KIM SUYEON <suyk52@gmail.com>
2025-06-13 00:22:08 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 256.25 ms, Memory: 11.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.py
@@ -0,0 +1,25 @@
+def solution(arr):
+    answer = [0, 0]
+
+    def compress(x, y, size):
+        initial = arr[x][y]
+        all_same = True
+
+        for i in range(x, x + size):
+            for j in range(y, y + size):
+                if arr[i][j] != initial:
+                    all_same = False
+                    break
+            if not all_same:
+                break
+        if all_same:
+            answer[initial] += 1
+        else:
+            half = size // 2
+            compress(x, y, half)
+            compress(x, y + half, half)
+            compress(x + half, y, half)
+            compress(x + half, y + half, half)
+
+    compress(0, 0, len(arr))
+    return answer

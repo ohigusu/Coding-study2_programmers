49877de8e370d18b248723a0a6b99cf788ff21c4
KIM SUYEON <suyk52@gmail.com>
2025-07-04 09:49:00 +0900

    [level 1] Title: 최소직사각형, Time: 7.81 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -1,2 +1,7 @@
 def solution(sizes):
-    return max(max(x) for x in sizes)*max(min(x) for x in sizes)
+    mini,maxi = [],[]
+    for size in sizes:
+        mini.append(min(size))
+        maxi.append(max(size))
+    return max(mini)*max(maxi)
+    #return max(max(x) for x in sizes)*max(min(x) for x in sizes)

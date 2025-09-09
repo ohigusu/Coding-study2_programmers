df11fa593fa39248d103548aa48c29bfc5640ca9
KIM SUYEON <suyk52@gmail.com>
2025-09-10 00:06:15 +0900

    [level 2] Title: 타겟 넘버, Time: 379.00 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,12 +1,14 @@
 def solution(numbers, target):
-    answer = 0
-    def dfs(idx,now):
+    answer = 0 # 숫자를 적절히 더하고 빼서 타겟 넘버를 만드는 방법의 수
+    def dfs(i,acc):
         nonlocal answer
-        if len(numbers)==idx:
-            if now == target:
+        if i == len(numbers):
+            if acc == target:
                 answer += 1
             return
-        dfs(idx+1,now - numbers[idx])
-        dfs(idx+1,now+numbers[idx])
+        # i번째 숫자를 더하는 경우
+        dfs(i + 1, acc + numbers[i])
+        # i번째 숫자를 빼는 경우
+        dfs(i + 1, acc - numbers[i])
     dfs(0,0)
-    return answer
+    return answer 

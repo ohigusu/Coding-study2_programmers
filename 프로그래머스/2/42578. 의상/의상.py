bccd49f99383f75c3ec87df119ffc30ea769b0fe
KIM SUYEON <suyk52@gmail.com>
2025-12-07 03:41:46 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,13 +1,12 @@
 def solution(clothes):
+    hash = {}
     answer = 1
-    appendix = {}
-    for i in range(len(clothes)):
-        kind = clothes[i][1]
-        if kind in appendix:
-            appendix[kind] += 1
+    for cloth,type in clothes:
+        if type in hash:
+            hash[type] += 1
         else:
-            appendix[kind] = 2 # +2를 하는 이유는 “입지 않음”까지 포함하기 때문에
-    for _,val in appendix.items():
-        answer *= val
-    answer -= 1 # 전부 "입지 않음"을 했을 경우 제외
-    return answer
+            hash[type] = 1
+    for key,value in hash.items():
+        answer *= (value+1)
+    return answer-1
+        
